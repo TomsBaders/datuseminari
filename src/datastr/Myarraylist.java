80e5f13fcca58cee9bacfc1b59003af0b5ae2eb1
Toms Bāders <s24badetoms@vea.lv>
2026-02-19 11:37:47 +0200

    pievienoti mainigie myarraylist.java

--- src/datastr/Myarraylist.java
+++ src/datastr/Myarraylist.java
@@ -1,5 +1,9 @@
 package datastr;
 
 public class Myarraylist {
-
+	
+	private char[] list;
+	private int howManyElements = 0;
+	private final int DEFAULT_SIZE = 8;
+	private int size = DEFAULT_SIZE;
 }
